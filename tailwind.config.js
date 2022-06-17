e9e20edb1b232b462933c8e026d9be6742eb6352
Andrew Disley <andrew@simplified.co.uk>
2022-06-17 15:21:49 +0100

    Content updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,26 @@ module.exports = {
   content: [
     './src/_includes/components/**/*.{html,js,njk}',
     './src/_includes/**/*.{html,js,njk}',
-    './src/**/*.{html,js,njk}'
+    './src/**/*.{html,js,njk}',
   ],
   theme: {
     container: {
       center: true,
     },
     extend: {
+      colors: {
+        superfunk: {
+          dark: '#121212',
+          'dark-2': '#292929',
+          blue: '#2399CA',
+          'blue-light': '#27AAE1',
+          green: '#7EB239',
+          'green-light': '#8CC63F',
+          pink: '#E53E94',
+          'pink-light': '#E53E94',
+          yellow: '#FFEB00',
+        },
+      },
     },
   },
   plugins: [],
